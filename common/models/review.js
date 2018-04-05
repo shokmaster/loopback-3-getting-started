c80f8b19e9d047e29dbba28d4f5f63dec6cd9422
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-04-05 21:12:52 +0200

    Until: Define a remote hook

--- common/models/review.js
+++ common/models/review.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// This function is called before a new instance of the Review model is created. The code:
+//  - Inserts the publisherId using the access token attached to the request.
+//  - Sets the date of the review instance to the current date.
 module.exports = function(Review) {
-
+  Review.beforeRemote('create', function(context, user, next) {
+    context.args.data.date = Date.now();
+    context.args.data.publisherId = context.req.accessToken.userId;
+    next();
+  });
 };
