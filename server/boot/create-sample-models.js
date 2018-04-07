a289dc2fbe0adf942ad928b2e8b8370f0e7787cb
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-04-07 21:08:57 +0200

    [back] Work with MySQL. Add models for Product and UserProduct. Set migrations and seeds.

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,30 +1,141 @@
 'use strict';
 
+// DOC: https://loopback.io/doc/en/lb3/Creating-a-default-admin-user.html
+
 var async = require('async');
 
 module.exports = function(app) {
+  const models = app.models;
+
+  logModels(models);
+
   // data sources
-  var mongoDs = app.dataSources.mongoDs; // 'name' of your mongo connector, you can find it in datasources.json
   var mysqlDs = app.dataSources.mysqlDs; // 'name' of your mysql connector, you can find it in datasources.json
+
+  mysqlDs.automigrate(['RoleMapping', 'AccessToken'], function(err) {
+    if (err) throw err;
+  });
 
   // create all models
   async.parallel({
     reviewers: async.apply(createReviewers),
     coffeeShops: async.apply(createCoffeeShops),
+    users: async.apply(createUsers),
   }, function(err, results) {
     if (err) throw err;
 
     createReviews(results.reviewers, results.coffeeShops, function(err) {
+      if (err) throw err;
+
       console.log('> models created sucessfully');
+    });
+
+    // [WIP] Create products, assign product owners
+    /*createProducts(results.users, function(err, products) {
+      if (err) throw err;
+
+      console.log('Created products:', products);
+      console.log('Pendiente crear UserProducts');
+
+      mysqlDs.automigrate('UserProduct', function(err) {
+        if (err) throw err;
+
+        const UserProduct = models.UserProduct;
+
+        UserProduct.create({
+          userId: 0,
+          productId: products[0].id,
+          code: '123abc',
+        });
+        //results.users[0].userProducts.create({
+        //  productId: products[0].id,
+        //  code: '123abc',
+        //});
+      });
+    });*/
+
+    // Create the admin role
+    createRoleAdmin(results.users, function(err, role) {
+      if (err) throw err;
+
+      console.log('Created role:', role);
+
+      const RoleMapping = models.RoleMapping;
+
+      // Make Juanan an admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: results.users[0].id,
+      }, function(err, principal) {
+        if (err) throw err;
+
+        console.log('Created principal:', principal);
+      });
     });
   });
 
-  // Creates the Reviewer data structure in MongoDB using auto-migration and adds data to it.
-  function createReviewers(cb) {
-    mongoDs.automigrate('Reviewer', function(err) {
+  function createProducts(users, cb) {
+    mysqlDs.automigrate('Product', function(err) {
       if (err) return cb(err);
 
-      var Reviewer = app.models.Reviewer;
+      const Product = models.Product;
+
+      Product.create([{
+        name: 'Panda Starter Kit',
+        reference: '0000',
+      }, {
+        name: 'Papagallo Starter Kit',
+        reference: '0001',
+      }, {
+        name: 'Urraca Starter Kit',
+        reference: '0002',
+      }], cb);
+    });
+  };
+
+  function createRoleAdmin(users, cb) {
+    console.log('Created users:', users);
+
+    mysqlDs.automigrate('Role', function(err) {
+      if (err) return cb(err);
+
+      const Role = models.Role;
+
+      Role.create({
+        name: 'admin',
+        description: 'admin of the world',
+      }, cb);
+    });
+  }
+
+  function createUsers(cb) {
+    mysqlDs.automigrate('User', function(err) {
+      if (err) return cb(err);
+
+      const User = models.User;
+
+      User.create([{
+        username: 'Juanan',
+        email: 'shokmaster@gmail.com',
+        password: '12345678',
+      }, {
+        username: 'Jane',
+        email: 'jane@doe.com',
+        password: '12345678',
+      }, {
+        username: 'Bob',
+        email: 'bob@projects.com',
+        password: '12345678',
+      }], cb);
+    });
+  }
+
+  // Creates the Reviewer data structure in MongoDB using auto-migration and adds data to it.
+  function createReviewers(cb) {
+    mysqlDs.automigrate('Reviewer', function(err) {
+      if (err) return cb(err);
+
+      var Reviewer = models.Reviewer;
 
       Reviewer.create([{
         email: 'foo@bar.com',
@@ -44,7 +155,7 @@ module.exports = function(app) {
     mysqlDs.automigrate('CoffeeShop', function(err) {
       if (err) return cb(err);
 
-      var CoffeeShop = app.models.CoffeeShop;
+      var CoffeeShop = models.CoffeeShop;
 
       CoffeeShop.create([{
         name: 'Bel Cafe',
@@ -61,10 +172,10 @@ module.exports = function(app) {
 
   // Creates the Reviews data structure in MongoDB using auto-migration and adds data to it.
   function createReviews(reviewers, coffeeShops, cb) {
-    mongoDs.automigrate('Review', function(err) {
+    mysqlDs.automigrate('Review', function(err) {
       if (err) return cb(err);
 
-      var Review = app.models.Review;
+      var Review = models.Review;
       var DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
 
       Review.create([{
@@ -94,4 +205,11 @@ module.exports = function(app) {
       }], cb);
     });
   }
+};
+
+const logModels = (models) => {
+  const modelsNames = Object.keys(models);
+
+  console.log(`Found ${modelsNames.length} models:`);
+  modelsNames.forEach((name) => console.log(`  - ${name}`));
 };
